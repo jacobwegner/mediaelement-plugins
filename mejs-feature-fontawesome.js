77177653a130735646589a5264e9c2418075e779
Jacob Wegner <jacobwegner@gmail.com>
2014-12-12 12:40:29 -0600

    Support fa-* prefix
    Font Awesome 4.0 moved from
    
      icon-*
       to
      fa fa-*
    
    for its CSS classes

--- mejs-feature-fontawesome.js
+++ mejs-feature-fontawesome.js
@@ -9,11 +9,11 @@
 (function($){
   MediaElementPlayer.prototype.buildfontawesome = function(player, controls, layers, media){
     // Speed Up: Make elements and add their class the right way, but ugly.
-    $('.mejs-volume-button button', controls).append('<i class="icon-volume-up"></i><i class="icon-volume-off"></i>');
-    $('.mejs-playpause-button button', controls).append('<i class="icon-play"></i><i class="icon-pause"></i>');
-    $('.mejs-stop-button button', controls).append('<i class="icon-stop"></i>');
-    $('.mejs-fullscreen-button button', controls).append('<i class="icon-fullscreen"></i>');
-    $('.mejs-unfullscreen-button button', controls).append('<i class="icon-resize-small"></i>');
-    $('.mejs-loop-button button', controls).append('<i class="icon-repeat"></i>');
+    $('.mejs-volume-button button', controls).append('<i class="fa fa-volume-up"></i><i class="fa fa-volume-off"></i>');
+    $('.mejs-playpause-button button', controls).append('<i class="fa fa-play"></i><i class="fa fa-pause"></i>');
+    $('.mejs-stop-button button', controls).append('<i class="fa fa-stop"></i>');
+    $('.mejs-fullscreen-button button', controls).append('<i class="fa fa-fullscreen"></i>');
+    $('.mejs-unfullscreen-button button', controls).append('<i class="fa fa-resize-small"></i>');
+    $('.mejs-loop-button button', controls).append('<i class="fa fa-repeat"></i>');
   };
 })(jQuery);
